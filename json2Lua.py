e9a2d9e486323476ebb7fbc47320ceb55f86d439
MZIChenjl <macchenjl@foxmail.com>
2017-10-20 16:42:07 +0800

    Try to handle the equip
    add func to handle the slot and equip
    fix some bugs in the luatable definition

--- json2Lua.py
+++ json2Lua.py
@@ -22,52 +22,68 @@ def _getWikiId(wikiShipRemodel, prevOrNext):
         return -1
     else:
         return wikiShips[wikiShipRemodel[prevOrNext]]['wiki_id']
+# Tranform equip list to string , eg.'a,b,c,d'
+def equip2str(equip):
+    formated_equip = []
+    for e in equip:
+        if isinstance(e, str):
+            formated_equip.append('-1')
+        else:
+            formated_equip.append(str(e))
+    return ','.join(formated_equip)
+
+# Tranform slot list to string, eg.'a,b,c,d'
+def slot2str(slot):
+    formated_slot = []
+    for s in slot:
+        formated_slot.append(str(s))
+    return ','.join(formated_slot)
+
+def safe_suffix(suffix, lan):
+    if not suffix:
+        return ''
+    return ship_namesuffix[suffix][lan]
 
 # Generate one entry of the lua table
 # from two sources: 'zh.kcwiki.org' and 'Who calls the fleet'.
 def generate(wikiShip, wctfShip):
     luaTableEntryStr = ""
-    luaTableEntryStr +=  '   ["{}"] = {{'.format(wikiShip['wiki_id']) + '\n'
-    luaTableEntryStr +=  '       ["ID"] = {},'.format(wctfShip['id']) + '\n'
-    luaTableEntryStr +=  '       ["图鉴号"] = {},'.format(wctfShip['no']) + '\n'
-    luaTableEntryStr +=  '       ["日文名"] = "{}",'.format(wctfShip['name']['ja_jp'] + ship_namesuffix[wctfShip['name']['suffix']]['ja_jp']) + '\n'
-    luaTableEntryStr +=  '       ["假名"] = "{}",'.format(wctfShip['name']['ja_kana'] + ship_namesuffix[wctfShip['name']['suffix']]['ja_kana']) + '\n'
-    luaTableEntryStr +=  '       ["中文名"] = "{}",'.format(wctfShip['name']['zh_cn'] + ship_namesuffix[wctfShip['name']['suffix']]['zh_cn']) + '\n'
-    luaTableEntryStr +=  '       ["舰种"] = {},'.format(stypeMap[wctfShip['type']]) + '\n'
-    luaTableEntryStr +=  '       ["级别"] = {{"{}",{}}},'.format(ship_classes[wctfShip['class']]['zh_cn'] + '型', wctfShip['class_no']) + '\n'
-    luaTableEntryStr +=  '       ["数据"] = {' + '\n'
-    luaTableEntryStr +=  '           ["耐久"] = {{{},{}}},'.format(wctfShip['stats']['hp'], wctfShip['hp_max']) + '\n'
-    luaTableEntryStr +=  '           ["火力"] = {{{},{}}},'.format(wctfShip['stats']['fire'], wctfShip["fire_max"]) + '\n'
-    luaTableEntryStr +=  '           ["雷装"] = {{{},{}}},'.format(wctfShip['stats']['torpedo'], wctfShip["torpedo_max"]) + '\n'
-    luaTableEntryStr +=  '           ["对空"] = {{{},{}}},'.format(wctfShip['stats']['aa'], wctfShip["aa_max"]) + '\n'
-    luaTableEntryStr +=  '           ["装甲"] = {{{},{}}},'.format(wctfShip['stats']['armor'], wctfShip["armor_max"]) + '\n'
-    luaTableEntryStr +=  '           ["对潜"] = {{{},{}}},'.format(wctfShip['stats']['asw'], wctfShip["asw_max"]) + '\n'
-    luaTableEntryStr +=  '           ["回避"] = {{{},{}}},'.format(wctfShip['stats']['evasion'], wctfShip["evasion_max"]) + '\n'
-    luaTableEntryStr +=  '           ["索敌"] = {{{},{}}},'.format(wctfShip['stats']['los'], wctfShip["los_max"]) + '\n'
-    luaTableEntryStr +=  '           ["运"] = {{{},{}}},'.format(wctfShip['stats']['luck'], wctfShip["luck_max"]) + '\n'
-    luaTableEntryStr +=  '           ["速力"] = {{{},{}}},'.format(wctfShip['stat']['speed']) + '\n'
-    luaTableEntryStr +=  '           ["射程"] = {{{},{}}},'.format(wctfShip['stat']['range']) + '\n'
-    luaTableEntryStr +=  '           ["稀有度"] = {{{},{}}}'.format(wctfShip['rare']) + '\n'
-    luaTableEntryStr +=  '       }},'
-    luaTableEntryStr +=  '       ["装备"] = {{'
-    slot = len(wctfShip['slot'])
-    if slot == 0:
-        luaTableEntryStr +=  '           ["格数"] = {{{}}},'.format(slot) + '\n'
-        luaTableEntryStr +=  '           ["搭载"] = {{}},'
-        luaTableEntryStr +=  '           ["初期装备"] = {{}}'
-    elif slot == 1:
-        luaTableEntryStr +=  '           ["格数"] = {{{}}},'.format(slot) + '\n'
-        luaTableEntryStr +=  '           ["搭载"] = {{{}}},'.format(wctfShip['slot'][0]) + '\n'
-        luaTableEntryStr +=  '           ["初期装备"] = {{{}}}'.format(wikiShip['']) + '\n'
-    luaTableEntryStr +=  '       }},' + '\n'
-    luaTableEntryStr +=  '       ["获得"] = {["掉落"] = 1,["改造"] = 0,["建造"] = 1,["时间"] = {}},'.format(, wctfShip['buildtime']) + '\n'
-    luaTableEntryStr +=  '       ["消耗"] = {{["燃料"] = {},["弹药"] = {}}},'.format(wctfShip['consum']['fuel'], wctfShip['consum']['ammo']) + '\n'
-    luaTableEntryStr +=  '       ["改修"] = {{["火力"] = {},["雷装"] = {},["对空"] = {},["装甲"] = {}}},'.format(wctfShip['modernization'][0], wctfShip['modernization'][1], wctfShip['modernization'][2], wctfShip['modernization'][3]) + '\n'
-    luaTableEntryStr +=  '       ["解体"] = {{["燃料"] = {},["弹药"] = {},["钢材"] = {},["铝"] = {}}},'.format(wctfShip['scrap'][0], wctfShip['scrap'][1],wctfShip['scrap'][2],wctfShip['scrap'][3])
-    luaTableEntryStr +=  '       ["改造"] = {["等级"] = {},["弹药"] = {},["钢材"] = {},["改造前"] = "-1",["改造后"] = "001a"},'.format(wctfShip['remodel']['next_lvl'], wctfShip['remodel_cost']['ammo'], wctfShip['remodel_cost']['steel'], _getWikiId(wctfShip['remodel'], 'prev'), _getWikiId(wctfShip['remodel'], 'next')) + '\n'
-    luaTableEntryStr +=  '       ["画师"] = "{}",'.format(entities[wctfShip['rels'['cv']]]) + '\n'
-    luaTableEntryStr +=  '       ["声优"] = "{}"'.format(entities[wctfShip['rels']['illustrator']]) + '\n'
-    luaTableEntryStr +=  '   },' + '\n'
+    luaTableEntryStr +=  '   ["{}"] = {{\n'.format(wikiShip['wiki_id'])
+    luaTableEntryStr +=  '       ["ID"] = {},\n'.format(wctfShip['id'])
+    luaTableEntryStr +=  '       ["图鉴号"] = {},\n'.format(wctfShip['no'])
+    luaTableEntryStr +=  '       ["日文名"] = "{}",\n'.format(wctfShip['name']['ja_jp'] + safe_suffix(wctfShip['name']['suffix'],'ja_jp'))
+    luaTableEntryStr +=  '       ["假名"] = "{}",\n'.format(wctfShip['name']['ja_kana'] + safe_suffix(wctfShip['name']['suffix'],'ja_kana'))
+    luaTableEntryStr +=  '       ["中文名"] = "{}",\n'.format(wctfShip['name']['zh_cn'] + safe_suffix(wctfShip['name']['suffix'],'zh_cn'))
+    luaTableEntryStr +=  '       ["舰种"] = {},\n'.format(stypeMap[wctfShip['type']])
+    luaTableEntryStr +=  '       ["级别"] = {{"{}",{}}},\n'.format(ship_classes[wctfShip['class']]['name']['zh_cn'] + '型', wctfShip['class_no'])
+    luaTableEntryStr +=  '       ["数据"] = {\n'
+    luaTableEntryStr +=  '           ["耐久"] = {{{},{}}},\n'.format(wctfShip['stat']['hp'], wctfShip['stat']['hp_max'])
+    luaTableEntryStr +=  '           ["火力"] = {{{},{}}},\n'.format(wctfShip['stat']['fire'], wctfShip['stat']['fire_max'])
+    luaTableEntryStr +=  '           ["雷装"] = {{{},{}}},\n'.format(wctfShip['stat']['torpedo'], wctfShip['stat']['torpedo_max'])
+    luaTableEntryStr +=  '           ["对空"] = {{{},{}}},\n'.format(wctfShip['stat']['aa'], wctfShip['stat']['aa_max'])
+    luaTableEntryStr +=  '           ["装甲"] = {{{},{}}},\n'.format(wctfShip['stat']['armor'], wctfShip['stat']['armor_max'])
+    luaTableEntryStr +=  '           ["对潜"] = {{{},{}}},\n'.format(wctfShip['stat']['asw'], wctfShip['stat']['asw_max'])
+    luaTableEntryStr +=  '           ["回避"] = {{{},{}}},\n'.format(wctfShip['stat']['evasion'], wctfShip['stat']['evasion_max'])
+    luaTableEntryStr +=  '           ["索敌"] = {{{},{}}},\n'.format(wctfShip['stat']['los'], wctfShip['stat']['los_max'])
+    luaTableEntryStr +=  '           ["运"] = {{{},{}}},\n'.format(wctfShip['stat']['luck'], wctfShip['stat']['luck_max'])
+    luaTableEntryStr +=  '           ["速力"] = {},\n'.format(wctfShip['stat']['speed'])
+    luaTableEntryStr +=  '           ["射程"] = {},\n'.format(wctfShip['stat']['range'])
+    luaTableEntryStr +=  '           ["稀有度"] = {}\n'.format(wctfShip['rare'])
+    luaTableEntryStr +=  '       }},\n'
+    luaTableEntryStr +=  '       ["装备"] = {{\n'
+    slot_size = len(wctfShip['slot'])
+    luaTableEntryStr +=  '           ["格数"] = {},\n'.format(slot_size)
+    luaTableEntryStr +=  '           ["搭载"] = {{{0}}},\n'.format(slot2str(wctfShip['slot']))
+    luaTableEntryStr +=  '           ["初期装备"] = {{{0}}}\n'.format(equip2str(wctfShip['equip']))
+    luaTableEntryStr +=  '       }},\n'
+    luaTableEntryStr +=  '       ["获得"] = {{["掉落"] = 1,["改造"] = 0,["建造"] = 1,["时间"] = {0}}},\n'.format(wctfShip['buildtime'])
+    luaTableEntryStr +=  '       ["消耗"] = {{["燃料"] = {},["弹药"] = {}}},\n'.format(wctfShip['consum']['fuel'], wctfShip['consum']['ammo'])
+    luaTableEntryStr +=  '       ["改修"] = {{["火力"] = {},["雷装"] = {},["对空"] = {},["装甲"] = {}}},\n'.format(wctfShip['modernization'][0], wctfShip['modernization'][1], wctfShip['modernization'][2], wctfShip['modernization'][3])
+    luaTableEntryStr +=  '       ["解体"] = {{["燃料"] = {},["弹药"] = {},["钢材"] = {},["铝"] = {}}},\n'.format(wctfShip['scrap'][0], wctfShip['scrap'][1],wctfShip['scrap'][2],wctfShip['scrap'][3])
+    luaTableEntryStr +=  '       ["改造"] = {["等级"] = {},["弹药"] = {},["钢材"] = {},["改造前"] = "-1",["改造后"] = "001a"},\n'.format(wctfShip['remodel']['next_lvl'], wctfShip['remodel_cost']['ammo'], wctfShip['remodel_cost']['steel'], _getWikiId(wctfShip['remodel'], 'prev'), _getWikiId(wctfShip['remodel'], 'next'))
+    luaTableEntryStr +=  '       ["画师"] = "{}",\n'.format(entities[wctfShip['rels']['illustrator']]['name']['ja_jp'])
+    luaTableEntryStr +=  '       ["声优"] = "{}"\n'.format(entities[wctfShip['rels']['cv']]['name']['ja_jp'])
+    luaTableEntryStr +=  '   },\n'
     return luaTableEntryStr
 
 def main():
