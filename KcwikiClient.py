87f7c03f55b980ca78bd109de44263009ccad9ae
MZI <macchenjl@foxmail.com>
2018-03-20 22:31:58 +0800

    add cache-control

--- KcwikiClient.py
+++ KcwikiClient.py
@@ -14,6 +14,7 @@ class KcwikiClient:
         self.loginToken = ''
         self.editToken = ''
         headers = {
+            'Cache-Control': 'no-cache',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36'
         }
 
